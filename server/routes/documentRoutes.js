4aa72473c72e042d31e99c97fa0f304089fcbe44
Ande UdayKiran <23eg112e02@anurag.edu.in>
2026-09-02 17:14:21 +0530

    feat: add share/unshare/collaborators routes

--- server/routes/documentRoutes.js
+++ server/routes/documentRoutes.js
@@ -1,5 +1,15 @@
 import { Router } from 'express';
-import { createDocument, deleteDocument, duplicateDocument, getDocument, listDocuments, updateDocument } from '../controllers/documentController.js';
+import {
+  createDocument,
+  deleteDocument,
+  duplicateDocument,
+  getDocument,
+  listCollaborators,
+  listDocuments,
+  shareDocument,
+  unshareDocument,
+  updateDocument
+} from '../controllers/documentController.js';
 import { protect } from '../middleware/authMiddleware.js';
 import { validateObjectIdParam } from '../middleware/objectIdMiddleware.js';
 
@@ -12,5 +22,10 @@ router.get('/:id', validateObjectIdParam('id'), getDocument);
 router.post('/:id/duplicate', validateObjectIdParam('id'), duplicateDocument);
 router.put('/:id', validateObjectIdParam('id'), updateDocument);
 router.delete('/:id', validateObjectIdParam('id'), deleteDocument);
+
+// Sharing
+router.get('/:id/collaborators', validateObjectIdParam('id'), listCollaborators);
+router.post('/:id/share', validateObjectIdParam('id'), shareDocument);
+router.delete('/:id/share/:userId', validateObjectIdParam('id'), validateObjectIdParam('userId'), unshareDocument);
 
 export default router;
